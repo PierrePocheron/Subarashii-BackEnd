66e0afc04cf9dbd724fe9968d7c7f9d9a1ddd414
PierrePocheron <pierre.pocheron@gmail.com>
2022-06-01 16:23:43 +0200

    fix: bad refactor

--- src/main/java/com/jfam/subarashii/entities/api/Result.java
+++ src/main/java/com/jfam/subarashii/entities/api/Result.java
@@ -3,17 +3,17 @@ package com.jfam.subarashii.entities.api;
 import java.util.ArrayList;
 
 public class Result {
-    public String backdropPath;
-    public String firstAirDate;
-    public ArrayList<Double> genreIds;
+    public String backdrop_path;
+    public String first_air_date;
+    public ArrayList<Double> genre_ids;
     public double id;
     public String name;
-    public ArrayList<String> originCountry;
-    public String originalLanguage;
-    public String originalName;
+    public ArrayList<String> origin_country;
+    public String original_language;
+    public String original_name;
     public String overview;
     public double popularity;
-    public String posterPath;
-    public double voteAverage;
-    public double voteCount;
+    public String poster_path;
+    public double vote_average;
+    public double vote_count;
 }
