2794b0cb64b38699984fb3b47b65ba38366421ce
PierrePocheron <pierre.pocheron@gmail.com>
2022-06-21 11:43:02 +0200

    test: init env test

---
+++ src/test/java/com/jfam/subarashii/utils/SubarashiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfam.subarashii.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubarashiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
